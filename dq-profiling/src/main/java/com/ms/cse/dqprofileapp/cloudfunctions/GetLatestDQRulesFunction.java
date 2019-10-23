321cb791a8395b6f7e1ec1c71b6999f584824c1d
Ryan CrawCour <ryancraw@microsoft.com>
2019-10-23 17:38:56 +0530

    added client skeleton

--- dq-profiling/src/main/java/com/ms/cse/dqprofileapp/cloudfunctions/GetLatestDQRulesFunction.java
+++ dq-profiling/src/main/java/com/ms/cse/dqprofileapp/cloudfunctions/GetLatestDQRulesFunction.java
@@ -1,15 +1,16 @@
 package com.ms.cse.dqprofileapp.cloudfunctions;
 
-import com.ms.cse.dqprofileapp.extensions.TimestampExtension;
-import com.ms.cse.dqprofileapp.models.RulesInfo;
-import com.ms.cse.dqprofileapp.repositories.RulesInfoRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
-import org.springframework.stereotype.Component;
-
 import java.sql.Timestamp;
 import java.util.List;
 import java.util.function.Function;
+
+import com.ms.cse.dqprofileapp.extensions.TimestampExtension;
+import com.ms.cse.dqprofileapp.models.RulesInfo;
+import com.ms.cse.dqprofileapp.repositories.RulesInfoRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
 
 @Component
 public class GetLatestDQRulesFunction {
@@ -20,6 +21,7 @@ public class GetLatestDQRulesFunction {
     public Function<Timestamp, List<RulesInfo>> getLatestDQRules() {
         return waterMarkDate -> {
             List<RulesInfo> rulesInfo = rulesInfoRepository.findByUpdateTimestampBetweenOrderByUpdateTimestampDesc(waterMarkDate, TimestampExtension.now());
+            
             return rulesInfo;
         };
     }
