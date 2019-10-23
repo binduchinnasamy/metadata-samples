7ae4c3ee4036b22c094081447d6e2bee81f66ef1
binduchinnasamy <bindukaladeepan@gmail.com>
2019-10-23 10:10:31 +0530

    updated databricks and python function

--- harvestor/adls-gen2/src/Function/EntitySync/ProcessEntity/__init__.py
+++ harvestor/adls-gen2/src/Function/EntitySync/ProcessEntity/__init__.py
@@ -6,35 +6,43 @@ import azure.functions as func
 
 
 def main(blob: func.InputStream):
-    logging.info(f"Python blob trigger function processed blob \n"
-                 f"Name: {blob.name}\n"
-                 f"Blob Size: {blob.length} bytes")
+    logging.info("Python blob trigger function processed blob")                
     blob_size=blob.length
     json_generator_url = os.environ["json_generator_url"]
     api_access_layer_url = os.environ["api_access_layer_url"]
-    qns_service_url=""
+    qns_service_url=os.environ["qns_url"]
 
     location_of_at=blob.name.find('@')
     location_of_root=blob.name.find('/')
     filesystem_name=blob.name[location_of_root+1:location_of_at]
     adls_gen2_url=blob.name[location_of_at+1:blob.name.find('.')]+".dfs.core.windows.net"
 
-    if blob_size >0:             
+    if blob_size >0: 
+
         json_object= json.loads(blob.read())
-        json_with_qns=validatewithQNS(json_object,qns_service_url,adls_gen2_url,filesystem_name)        
-        # call json generator service    
-        return_json=httpPostJsonGenerator(json_with_qns,json_generator_url)
-        if return_json.status_code == 200:
-            json_entity_objects=json.loads(return_json.text)
-            # call Json api access service
-            api_output = httpPostJsonGenerator(json_entity_objects,api_access_layer_url)
-            if api_output.status_code ==200:
-                logging.info("Entity created successfully!")
-            else:
-                logging.error("Error in calling meta access layer with error code:"+api_output.status_code + 
-                " "+api_output.text)
-    else:
-        logging.error("input request json is empty")   
+
+        storage_ac_json = json_object["azure_storage_account"]
+        filesystem_json = json_object["azure_datalake_gen2_filesystem"]
+        resource_set_json = json_object["azure_datalake_gen2_resource_set"]
+
+        final_list=[]
+        if resource_set_json and filesystem_json and resource_set_json:
+            resource_set_json_qns=validatewithQNS(resource_set_json,qns_service_url,adls_gen2_url,filesystem_name,'resourceset')
+            filesystem_json_qns=validatewithQNS(filesystem_json,qns_service_url,adls_gen2_url,filesystem_name,'filesystem')
+            final_list.append(resource_set_json_qns)
+            final_list.append(filesystem_json_qns)            
+            # call json generator service    
+            return_json=httpPostJsonGenerator(final_list,json_generator_url)
+            if return_json.status_code == 200:
+                json_entity_objects=json.loads(return_json.text)
+                # call Json api access service
+                api_output = httpPostJsonGenerator(json_entity_objects,api_access_layer_url)
+                if api_output.status_code ==200:
+                    logging.info("Entity created successfully!")
+                else:
+                    logging.error("Error in calling meta access layer with error code:"+api_output.status_code + " "+api_output.text)
+        else:
+            logging.error("input request json is empty")   
 
 def httpPostJsonGenerator(input_content, url):    
     try:        
@@ -46,23 +54,56 @@ def httpPostJsonGenerator(input_content, url):
     except requests.RequestException as RequestException:
         logging.error("Request Exception "+ RequestException)
 
-def validatewithQNS(input_content, qns_service_url, adls_gen2_uri, file_system):
-    for entity in input_content:
-        entity_path=entity['attributes'][0]['attr_value']
-        qualified_name=getQualifiedName(adls_gen2_uri,file_system,entity_path,qns_service_url)
-        entity['attributes'][0]['attr_name']=qualified_name
-    return input_content
+def validatewithQNS(input_content, qns_service_url, adls_gen2_uri, file_system, entity_type):    
+    if entity_type=='filesystem':
+        entity_with_qns=process_entity(input_content,adls_gen2_uri,file_system,qns_service_url, entity_type)
+        return entity_with_qns
+    elif entity_type=='resourceset':
+        for entity in input_content:
+            entity_with_qns=process_entity(entity,adls_gen2_uri,file_system,qns_service_url, entity_type)        
+        return entity_with_qns
 
-def getQualifiedName(adls_gen2_uri, file_system, entity_path, qns_service_url):
-    qns_request_obj ={
+def process_entity(entity,adls_gen2_uri,file_system,qns_service_url, entity_type):
+    entity_path=entity['attributes'][1]['attr_value']
+    response=getQualifiedName(adls_gen2_uri,file_system,entity_path,qns_service_url,entity_type)
+    if response.status_code == 200:
+
+        response_json=json.loads(response.text)        
+        if response_json['isExists']==False:
+            entity['attributes'][0]['attr_value'] =response_json['qualifiedName']
+        elif response_json['isExists']==True:
+            response_json['attributes'][0]['attr_value'] =response_json['qualifiedName']
+            guid_object ={
+                "guid":response_json['guid']
+            }
+            entity['attributes'].append(guid_object)        
+        return entity
+    else:
+        return None 
+def getQualifiedName(adls_gen2_uri, file_system, entity_path, qns_service_url,entity_type):
+    qns_request_obj ={}
+    if entity_type =='resourceset':
+        qns_request_obj ={
         "azure_storage_uri":adls_gen2_uri,
         "filesystem_name":file_system,
         "resource_set_path":entity_path
-    }
+        }
+        qns_service_url=qns_service_url+'&typeName=azure_datalake_gen2_resource_sets'
+    elif entity_type =='storageac':
+        qns_request_obj ={
+        "azure_storage_uri":adls_gen2_uri,
+        "filesystem_name":file_system,
+        "resource_set_path":entity_path
+        }
+        qns_service_url=qns_service_url+'&typeName=azure_storage_account'
+    elif entity_type =='filesystem':
+        qns_request_obj ={
+        "azure_storage_uri":adls_gen2_uri,
+        "filesystem_name":file_system        
+        }
+        qns_service_url=qns_service_url+'&typeName=azure_datalake_gen2_filesystem'
     headers = {'Content-type': 'application/json'}
-    #response=requests.post(qns_service_url,data=qns_request_obj,headers=headers)
-    #if response.status_code==200:
-    #    response_text = response.text
-    return "qualified_name"
+    response=requests.post(qns_service_url,data=json.dumps(qns_request_obj),headers=headers)                  
+    return response
 
     
--- harvestor/adls-gen2/src/adls-scanner.py
+++ harvestor/adls-gen2/src/adls-scanner.py
@@ -1,11 +1,21 @@
 # Databricks notebook source
+import os
+from azure.common.credentials import ServicePrincipalCredentials
+from azure.mgmt.resource import ResourceManagementClient
+from azure.mgmt.storage import StorageManagementClient
+from azure.storage.blob import BlockBlobService
+import json
+import time
+from datetime import datetime
+
+# COMMAND ----------
+
 # MAGIC %md
-# MAGIC This code block gets environment variables from spark cluster environment variables
+# MAGIC Get all the variables from cluster's environment variables. The variables should be pushed to the cluster via DevOps pipline in a ideal scenario
 
 # COMMAND ----------
 
 # Get Environment varaibles
-import os
 KeyVault_Scope = os.environ['Azure_KeyVault_Scope']
 KeyVault_ADLSGen2_Access_Secret_Name = os.environ['KeyVault_ADLSGen2_Access_Secret_Name']
 ADLSGen2_URL = os.environ['ADLSGen2_URL']
@@ -14,14 +24,18 @@ KeyVault_BlobStorage_Access_Secret_Name = os.environ['KeyVault_BlobStorage_Acces
 BlobStorage_URL = os.environ['BlobStorage_URL']
 BlobStorage_Output_Container = os.environ['BlobStorage_Output']
 Scan_Depth=os.environ['Scan_Depth']
+AAD_Client_Id=os.environ['AAD_Client_Id']
+KeyVault_Client_Secret_Secret_Name=os.environ['KeyVault_Client_Secret_Secret_Name']
+ADLSGen2_Resource_Group=os.environ['ADLSGen2_Resource_Group']
+ADLSGen2_Subscription_Id=os.environ['ADLSGen2_Subscription_Id']
+AAD_Tenant_Id=os.environ['AAD_Tenant_Id']
 
 # COMMAND ----------
 
 # MAGIC %md
-# MAGIC set spart configuration to access ADSL Gen 2
+# MAGIC Set spart configuration to access ADLS Gen2 file system. This python job using ADLS storage keys to access the file system from Spark.
 
 # COMMAND ----------
-
 
 spark.conf.set(
   "fs.azure.account.key."+ADLSGen2_URL,
@@ -30,7 +44,7 @@ spark.conf.set(
 # COMMAND ----------
 
 # MAGIC %md
-# MAGIC Set spark config to access Az blob storage account
+# MAGIC Set spark configuration to access Blob storage to store the output JSON file
 
 # COMMAND ----------
 
@@ -42,18 +56,15 @@ spark.conf.set(
 # COMMAND ----------
 
 # MAGIC %md
-# MAGIC Function to create a JSON file in Blob storage container
+# MAGIC Method to upload create file in Azure blob storage. <br>
+# MAGIC Inputs : Content, file name
 
 # COMMAND ----------
 
-# Function to create file in output Blob storage account
-import time
-from datetime import datetime
-
-def uploadtoBlob(content):
+def uploadtoBlob(content, file_name):
   try:
     timenow = datetime.now()
-    file_name = str(timenow.strftime("%m%d%Y-%H-%M-%S"))+".json"
+    file_name = file_name+str(timenow.strftime("-%m%d%Y-%H-%M-%S"))+".json"
     result = dbutils.fs.put("wasbs://"+BlobStorage_Output_Container+"@"+BlobStorage_URL+"/"+file_name,content,True)
     if result == True:
       print("File creation success!")
@@ -65,68 +76,194 @@ def uploadtoBlob(content):
 # COMMAND ----------
 
 # MAGIC %md
-# MAGIC Scanner method
+# MAGIC Method to make JSON in a needed format <br>
+# MAGIC Input - Python list <br>
+# MAGIC Output - Json String
 
 # COMMAND ----------
 
-root_path = "abfss://"+ADLSGen2_FileSystem+"@"+ADLSGen2_URL+"/"
-
-startlevel =1
-entitylist=[]
-scan_depth= int(Scan_Depth)
-def getpath(path, level):
-  try:
-    for file in dbutils.fs.ls(path):      
-      pathvalue = str(file.path)      
-      pathvalue_string = pathvalue.split(root_path)      
-      pathvalue_entity =pathvalue_string[-1]      
-      entitylist.append(pathvalue_entity)
-      if level < scan_depth:
-        newlevel= level+1        
-        getpath(file.path,newlevel)
-  except:
-    print("Error: Not able to access mount path:"+ path )
-
-# COMMAND ----------
-
-getpath(root_path,startlevel)
-
-# COMMAND ----------
-
-# MAGIC %md
-# MAGIC Create entity JSON file
-
-# COMMAND ----------
-
-import json
-
-entity_final=[]
-for entity in entitylist:
+def makeoutputjson_storageaccount(name,subscriptionId,resourceGroupName,location,createTime,SKU):
   entity_json ={
-    "entity_type_name": "adls_gen2_resource_set",
-    "created_by": "sg",
-    "attributes": [{
-		"attr_name": "qualifiedName",
-		"attr_value": entity,
-		"is_entityref": False
-     }, 
-      {
-		"attr_name": "name",
-		"attr_value": entity,
-		"is_entityref": False
-    }
-    ]
+    "entity_type_name":"azure_storage_account",
+    "created_by":"harvester",
+     "attributes":[
+         {
+             "attr_name":"qualifiedname",
+             "attr_value":""
+         },
+         {
+            "attr_name":"name",
+            "attr_value":name
+        },
+        {
+            "attr_name":"subscriptionId",
+            "attr_value":subscriptionId
+        }, 
+        {
+            "attr_name":"resourceGroupName",
+            "attr_value":resourceGroupName
+        },
+        {
+            "attr_name":"location",
+            "attr_value":location
+        },
+        {
+            "attr_name":"createTime",
+            "attr_value":createTime
+        },
+        {
+            "attr_name":"accessTier",
+            "attr_value":"Unknown"
+        },
+        {
+            "attr_name":"SKU",
+            "attr_value":SKU
+        },
+        {
+            "attr_name":"kind",
+            "attr_value":"StorageV2"
+        }
+     ]
   }
-  entity_final.append(entity_json)
+  json_string= json.dumps(entity_json)
+  return json_string
+
+# COMMAND ----------
+
+def makeoutputjson_filesystem(name):
+  entity_json ={
+      "entity_type_name": "azure_datalake_gen2_filesystem",
+      "created_by": "harvester",
+      "attributes": [{
+          "attr_name": "qualifiedName",
+          "attr_value": "",
+          "is_entityref": False
+       }, 
+        {
+          "attr_name": "name",
+          "attr_value": name,
+          "is_entityref": False
+      }
+      ]
+    }
+  json_string= json.dumps(entity_json)
+  return json_string
   
-json_string= json.dumps(entity_final)
-print(json_string)
+
+# COMMAND ----------
+
+def makeoutputjson(entitylist):
+  entity_final=[]
+  for entity in entitylist:
+    entity_json ={
+      "entity_type_name": "azure_datalake_gen2_resource_set",
+      "created_by": "harvester",
+      "attributes": [{
+          "attr_name": "qualifiedName",
+          "attr_value": "",
+          "is_entityref": False
+       }, 
+        {
+          "attr_name": "name",
+          "attr_value": entity,
+          "is_entityref": False
+      }
+      ]
+    }
+    entity_final.append(entity_json)
+  json_string= json.dumps(entity_final)
+  return json_string
 
 # COMMAND ----------
 
 # MAGIC %md
-# MAGIC Upload generated JSON to Azure Blob Storage
+# MAGIC Method to scan the ADLS Gen 2 file system folders recursively using databricks dbutils
 
 # COMMAND ----------
 
-uploadtoBlob(json_string)
+def getpath(path, level, entitylist, root_path ):  
+  files = dbutils.fs.ls(path)
+  for file in files:    
+    pathvalue = str(file.path)      
+    pathvalue_string = pathvalue.split(root_path)      
+    pathvalue_entity =pathvalue_string[-1]    
+    entitylist.append(pathvalue_entity)
+    if level <= int(Scan_Depth):
+        newlevel= level+1        
+        getpath(file.path,newlevel,entitylist,root_path)
+  return entitylist
+
+# COMMAND ----------
+
+ 
+def scanfilesystem(filesystem_name, account_url):  
+  entitylist=[]
+  startlevel =1
+  try:    
+    root_path = "abfss://"+filesystem_name+"@"+account_url+"/"
+    entitylist = getpath(root_path,startlevel,entitylist,root_path)
+  except:
+    print('Error in scan file system')
+  finally:
+    return entitylist
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC <b>Main method</b> <br>
+# MAGIC This script is using Azure serice principal to get access to the storage account properties <br>
+# MAGIC Service principal client id is stored in environment variable and secrets are pulled from Azure KeyVault <br><br>
+# MAGIC This script doesn't mount ADLS Gen 2 file system to databricks, instead it directly access the file system
+
+# COMMAND ----------
+
+subscription_id =  ADLSGen2_Subscription_Id
+ad_client_id=AAD_Client_Id
+ad_client_secret=dbutils.secrets.get(scope = KeyVault_Scope, key =KeyVault_Client_Secret_Secret_Name ) 
+ad_tenantid=AAD_Tenant_Id
+#
+resource_group_name=ADLSGen2_Resource_Group
+storage_account_name=ADLSGen2_URL[0:ADLSGen2_URL.find('.')]
+#Make credential object
+credentials = ServicePrincipalCredentials(client_id=ad_client_id, secret=ad_client_secret, tenant=ad_tenantid)    
+resource_client = ResourceManagementClient(credentials, subscription_id)
+storage_client = StorageManagementClient(credentials, subscription_id)
+storage_account = storage_client.storage_accounts.get_properties(resource_group_name, storage_account_name)
+# Get properties of storage account
+# Get properties of storage account
+sa_creation_time=storage_account.creation_time.strftime("%Y-%m-%d %H:%M:%S")
+sa_kind=storage_account.kind
+sa_location=storage_account.location
+sa_name=storage_account.name
+sa_sku=storage_account.sku.name
+print(sa_sku)
+# Go further only for Storage Gen 2
+if sa_kind =='StorageV2':
+  # make output json for storage account
+  output_json_sa = makeoutputjson_storageaccount(sa_name,subscription_id,resource_group_name,sa_location,sa_creation_time,sa_sku)
+  print(output_json_sa)
+  #Get Storage Account file system properties
+  storage_keys = storage_client.storage_accounts.list_keys(resource_group_name, storage_account_name)
+  storage_keys = {v.key_name: v.value for v in storage_keys.keys}
+  block_blob_service = BlockBlobService(account_name=storage_account_name, account_key=storage_keys['key1'])
+  containers = block_blob_service.list_containers()
+  filesystems=[]
+  # Get file system names
+  for container in containers:
+      filesystems.append(container.name)
+  for filesystem in filesystems:  
+    # Pull the entity
+    # make output json for file system
+    output_json_fs= makeoutputjson_filesystem(filesystem)    
+    #print(output_json_fs)
+    entitylist= scanfilesystem(filesystem,ADLSGen2_URL)
+    if len(entitylist) >0:
+      output_json= makeoutputjson(entitylist)
+      output_json_filesystem={
+        "azure_storage_account":json.loads(output_json_sa),
+        "azure_datalake_gen2_filesystem":json.loads(output_json_fs),
+        "azure_datalake_gen2_resource_set":json.loads(output_json)
+      }
+      json_string_final= json.dumps(output_json_filesystem)
+      output_filename=filesystem+"@"+ADLSGen2_URL
+      uploadtoBlob(json_string_final,output_filename)
