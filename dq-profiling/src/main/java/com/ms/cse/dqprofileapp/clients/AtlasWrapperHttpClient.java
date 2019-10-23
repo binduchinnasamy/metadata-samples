8a88d8b3e9ecada4ca57e427c886634ba0a2811b
Nischay Sharma <nischays@microsoft.com>
2019-10-24 01:38:38 +0530

    refactor

--- dq-profiling/src/main/java/com/ms/cse/dqprofileapp/clients/AtlasWrapperHttpClient.java
+++ dq-profiling/src/main/java/com/ms/cse/dqprofileapp/clients/AtlasWrapperHttpClient.java
@@ -1,6 +1,28 @@
 package com.ms.cse.dqprofileapp.clients;
 
+import java.util.logging.Logger;
+
 public class AtlasWrapperHttpClient{
 //url + "entity/bulk"
 //no code
+    private static AtlasWrapperHttpClient single_instance = null;
+
+    private Logger logger;
+    private String baseUrl;
+    private String funcCode;
+
+    private AtlasWrapperHttpClient(String baseUrl, String funcCode, Logger logger)
+    {
+        this.baseUrl = baseUrl;
+        this.funcCode = funcCode;
+        this.logger = logger;
+    }
+
+    public static AtlasWrapperHttpClient getInstance(String baseUrl, String funcCode, Logger logger)
+    {
+        if (single_instance == null)
+            single_instance = new AtlasWrapperHttpClient(baseUrl, funcCode, logger);
+
+        return single_instance;
+    }
 }
