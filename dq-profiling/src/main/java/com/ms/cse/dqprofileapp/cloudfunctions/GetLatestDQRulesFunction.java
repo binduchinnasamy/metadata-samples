a0a53aad2bf59c26de2b1a029950a8c3028ff51d
Ryan CrawCour <ryancraw@microsoft.com>
2019-10-24 11:03:23 +0530

    gitignoring

--- dq-profiling/src/main/java/com/ms/cse/dqprofileapp/cloudfunctions/GetLatestDQRulesFunction.java
+++ dq-profiling/src/main/java/com/ms/cse/dqprofileapp/cloudfunctions/GetLatestDQRulesFunction.java
@@ -7,6 +7,7 @@ import java.util.function.Function;
 
 import com.ms.cse.dqprofileapp.models.*;
 import kong.unirest.JsonNode;
+import kong.unirest.json.JSONObject;
 
 import com.ms.cse.dqprofileapp.clients.*;
 import com.ms.cse.dqprofileapp.repositories.RulesInfoRepository;
@@ -48,7 +49,8 @@ public class GetLatestDQRulesFunction {
             
             for (RulesInfo ruleInfo : rulesInfo) {                
                 QualifiedNameServiceResponse qualifiedNameResponse = qnsClient.getQualifiedName(ruleInfo.getColumnFQDN(), ruleInfo.getRuleId());
-
+                System.out.println("qualifiedNameResponse:" + qualifiedNameResponse.toString());
+                                
                 if (!qualifiedNameResponse.isExists()){
                     JsonWrapperEntity entity = JsonWrapperEntity.create(ruleInfo);
                     entities.add(entity);
@@ -60,11 +62,40 @@ public class GetLatestDQRulesFunction {
                 AtlasWrapperHttpClient atlasWrapperClient = AtlasWrapperHttpClient.getInstance(this.atlasWrapperSvcUrl, logger);
                 
                 JsonNode jsonWrapperEntities = jsonCreatorClient.getJson(JsonWrapperEntity.from(entities));
-                System.out.println(jsonWrapperEntities.toPrettyString());
+                System.out.println("jsonWrapperEntities:" + jsonWrapperEntities.toPrettyString());
                 
                 //take output json and call atlaswrapper create bulk entity
                 JsonNode mutatedEntities = atlasWrapperClient.createBulk(jsonWrapperEntities);
-                System.out.println(mutatedEntities.toPrettyString());
+                System.out.println("mutatedEntities: " + mutatedEntities.toPrettyString());
+
+                /*
+                {
+                    "mutatedEntities": {
+                        "CREATE": [{
+                            "typeName": "dq_rule",
+                            "attributes": {
+                                "rule_id": "rule1"
+                            },
+                            "guid": "0a3ea624-6bea-4322-bc49-92942d1297f1"
+                        }, {
+                            "typeName": "dq_rule",
+                            "attributes": {
+                                "rule_id": "rule2"
+                            },
+                            "guid": "07f09b27-f38f-4b5c-bec0-af7b54acccb3"
+                        }]
+                    },
+                    "guidAssignments": {
+                        "-77653153359712": "07f09b27-f38f-4b5c-bec0-af7b54acccb3",
+                        "-77653153359711": "0a3ea624-6bea-4322-bc49-92942d1297f1"
+                    }
+                }
+                */
+
+                //extract guids of created entities from mutatedEntities.guidAssignments
+                JSONObject guids = mutatedEntities.getObject().getJSONObject("mutatedEntities.guidAssignments");
+                
+                System.out.println("guids: " + guids.toString());
             }
 
             return rulesInfo;
