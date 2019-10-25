998067f7b4eaccf5d7c9015c9cf2ec879cbb2384
Nischay Sharma <nischays@microsoft.com>
2019-10-25 09:58:37 +0530

    remove non-required classes

--- dq-profiling/src/main/java/com/ms/cse/dqprofileapp/extensions/TimestampExtension.java
+++ dq-profiling/src/main/java/com/ms/cse/dqprofileapp/extensions/TimestampExtension.java
@@ -7,11 +7,23 @@ import java.time.temporal.TemporalAccessor;
 
 public class TimestampExtension {
 
+    public static Timestamp minTimestamp() {
+        String minDateAsISO8601 = "1970-01-01T00:00:00.000000+05:30";
+        DateTimeFormatter timeFormatter = DateTimeFormatter.ISO_DATE_TIME;
+        TemporalAccessor accessor = timeFormatter.parse(minDateAsISO8601);
+        Timestamp minTimestamp = Timestamp.from(Instant.from(accessor));
+        return minTimestamp;
+    }
+
     public static Timestamp fromInstant(Instant instant) {
         return Timestamp.valueOf(instant.toString().replace('T', ' ').replace("Z", ""));
     }
 
     public static Timestamp fromISO8601(String dateAsISO8601) {
+        if(dateAsISO8601.startsWith("0001-01-01")) {
+            return minTimestamp();
+        }
+
         DateTimeFormatter timeFormatter = DateTimeFormatter.ISO_DATE_TIME;
         TemporalAccessor accessor = timeFormatter.parse(dateAsISO8601);
         Timestamp timestamp = fromInstant(Instant.from(accessor));
